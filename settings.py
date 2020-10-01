2e372f1a01bbe3adafe5a138ae8d4d74a7eea078
William Jackson <william@subtlecoolness.com>
2020-10-01 12:24:48 -0500

    Extract Settings object to a separate module

---
+++ settings.py
@@ -0,0 +1,28 @@
+import os
+import pathlib
+
+
+class Settings:
+    @property
+    def external_id_file(self) -> pathlib.Path:
+        return pathlib.Path(os.getenv('EXTERNAL_ID_FILE', '/data.csv')).resolve()
+
+    @property
+    def log_format(self) -> str:
+        return os.getenv('LOG_FORMAT', '%(levelname)s [%(name)s] %(message)s')
+
+    @property
+    def log_level(self) -> str:
+        return os.getenv('LOG_LEVEL', 'INFO')
+
+    @property
+    def zendesk_company(self) -> str:
+        return os.getenv('ZENDESK_COMPANY')
+
+    @property
+    def zendesk_password(self) -> str:
+        return os.getenv('ZENDESK_PASSWORD')
+
+    @property
+    def zendesk_username(self) -> str:
+        return os.getenv('ZENDESK_USERNAME')
--- update-user-external-id.py
+++ update-user-external-id.py
@@ -1,57 +1,30 @@
 import csv
 import logging
-import os
-import pathlib
+import settings
 import sys
 import zendesk
 
 log = logging.getLogger(__name__)
 
 
-class Settings:
-    @property
-    def external_id_file(self) -> pathlib.Path:
-        return pathlib.Path(os.getenv('EXTERNAL_ID_FILE', '/data.csv')).resolve()
-
-    @property
-    def log_format(self) -> str:
-        return os.getenv('LOG_FORMAT', '%(levelname)s [%(name)s] %(message)s')
-
-    @property
-    def log_level(self) -> str:
-        return os.getenv('LOG_LEVEL', 'INFO')
-
-    @property
-    def zendesk_company(self) -> str:
-        return os.getenv('ZENDESK_COMPANY')
-
-    @property
-    def zendesk_password(self) -> str:
-        return os.getenv('ZENDESK_PASSWORD')
-
-    @property
-    def zendesk_username(self) -> str:
-        return os.getenv('ZENDESK_USERNAME')
-
-
-def get_external_ids(settings: Settings):
+def get_external_ids(s: settings.Settings):
     result = {}
-    with settings.external_id_file.open(newline='') as f:
+    with s.external_id_file.open(newline='') as f:
         for line in csv.DictReader(f):
             result[line.get('email').lower()] = line.get('external_id')
     return result
 
 
 def main():
-    settings = Settings()
-    logging.basicConfig(format=settings.log_format, level=logging.DEBUG, stream=sys.stdout)
-    if not settings.log_level == 'DEBUG':
-        log.debug(f'Changing log level to {settings.log_level}')
-    logging.getLogger().setLevel(settings.log_level)
+    s = settings.Settings()
+    logging.basicConfig(format=s.log_format, level=logging.DEBUG, stream=sys.stdout)
+    if not s.log_level == 'DEBUG':
+        log.debug(f'Changing log level to {s.log_level}')
+    logging.getLogger().setLevel(s.log_level)
 
-    log.info(f'Reading external_ids from {settings.external_id_file}')
-    external_ids = get_external_ids(settings)
-    z = zendesk.ZendeskClient(settings.zendesk_company, settings.zendesk_username, settings.zendesk_password)
+    log.info(f'Reading external_ids from {s.external_id_file}')
+    external_ids = get_external_ids(s)
+    z = zendesk.ZendeskClient(s.zendesk_company, s.zendesk_username, s.zendesk_password)
 
     for user in z.users:
         if user.suspended:
