86ff9792af006e9d2ae80792f7005ce56aefef97
William Jackson <565174+williamjacksn@users.noreply.github.com>
2023-02-06 14:14:58 -0600

    Add a script to change all agents to unrestricted

---
+++ update-agents.py
@@ -0,0 +1,14 @@
+import notch
+import os
+import zendesk
+
+log = notch.make_log('update-agents')
+
+company = os.getenv('ZENDESK_COMPANY')
+username = os.getenv('ZENDESK_USERNAME')
+password = os.getenv('ZENDESK_PASSWORD')
+z = zendesk.ZendeskClient(company, username, password)
+
+for user in z.users:
+    if user.role == 'agent' and user.restricted_agent:
+        user.ticket_restriction = None
