d5b9dba01c1f316f6e2ee0459ac0c33a61603f3d
William Jackson <565174+williamjacksn@users.noreply.github.com>
2023-02-06 14:13:07 -0600

    Update Zendesk client module

--- zendesk.py
+++ zendesk.py
@@ -2,10 +2,17 @@ import logging
 import requests
 import requests.auth
 
+from typing import Optional
+
 log = logging.getLogger(__name__)
 
 
 class ZendeskClient:
+    _groups = None
+    _organization_memberships = None
+    _organizations = None
+    _users = None
+
     def __init__(self, company: str, username: str, password: str):
         self.base_url = f'https://{company}.zendesk.com/api/v2'
         log.debug(f'Zendesk base url is {self.base_url}')
@@ -13,17 +20,39 @@ class ZendeskClient:
         self.s.auth = requests.auth.HTTPBasicAuth(username, password)
         self.s.headers.update({'accept': 'application/json'})
 
+    def _delete(self, url: str):
+        log.debug(f'DELETE {url}')
+        response = self.s.delete(url)
+        response.raise_for_status()
+        # return response.json()
+
     def _get(self, url: str, params: dict = None):
-        log.debug(f'Getting {url}')
+        log.debug(f'GET {url}')
         response = self.s.get(url, params=params)
         response.raise_for_status()
         return response.json()
 
+    def _post(self, url: str, json: dict):
+        log.debug(f'POST {url} / {json}')
+        response = self.s.post(url, json=json)
+        response.raise_for_status()
+        return response.json()
+
     def _put(self, url: str, json: dict):
-        log.debug(f'Putting {url} / {json}')
+        log.debug(f'PUT {url} / {json}')
         response = self.s.put(url, json=json)
         response.raise_for_status()
         return response.json()
+
+    def create_organization_membership(self, user_id: int, organization_id: int):
+        url = f'{self.base_url}/organization_memberships.json'
+        json = {
+            'organization_membership': {
+                'organization_id': organization_id,
+                'user_id': user_id,
+            },
+        }
+        return self._post(url, json)
 
     def get_incremental_tickets(self, start_time: int):
         _url = f'{self.base_url}/incremental/tickets/cursor.json'
@@ -33,6 +62,16 @@ class ZendeskClient:
         data = self._get(_url, params)
         yield from [ZendeskTicket(self, t) for t in data.get('tickets', [])]
 
+    def get_organization_by_id(self, organization_id: int) -> Optional['ZendeskOrganization']:
+        for org in self.organizations:
+            if org.id == organization_id:
+                return org
+
+    def get_organization_by_name(self, organization_name: str) -> Optional['ZendeskOrganization']:
+        for org in self.organizations:
+            if org.name == organization_name:
+                return org
+
     def get_ticket_field_options(self, field_id: int):
         _url = f'{self.base_url}/ticket_fields/{field_id}/options.json'
         while _url is not None:
@@ -40,6 +79,41 @@ class ZendeskClient:
             _url = data.get('next_page')
             _options = data.get('custom_field_options', [])
             yield from [ZendeskCustomFieldOption(self, o) for o in _options]
+
+    def get_user_by_id(self, user_id: int) -> Optional['ZendeskUser']:
+        for user in self.users:
+            if user.id == user_id:
+                return user
+
+    @property
+    def groups(self):
+        if self._groups is None:
+            result = []
+            url = f'{self.base_url}/groups.json'
+            params = {
+                'page[size]': 100,
+            }
+            has_more = True
+            while has_more:
+                data = self._get(url, params)
+                _groups = data.get('groups')
+                result.extend([ZendeskGroup(self, i) for i in _groups])
+                has_more = data.get('meta').get('has_more')
+                params.update({
+                    'page[after]': data.get('meta').get('after_cursor'),
+                })
+            self._groups = result
+        return self._groups
+
+    def list_memberships_for_org(self, organization_id: int):
+        for m in self.organization_memberships:
+            if m.organization_id == organization_id:
+                yield m
+
+    def list_memberships_for_user(self, user_id: int):
+        for m in self.organization_memberships:
+            if m.user_id == user_id:
+                yield m
 
     def list_user_identities(self, user_id: int):
         _url = f'{self.base_url}/users/{user_id}/identities.json'
@@ -51,28 +125,44 @@ class ZendeskClient:
 
     @property
     def organization_memberships(self):
-        _url = f'{self.base_url}/organization_memberships.json'
-        params = {
-            'page[size]': 100,
-        }
-        has_more = True
-        while has_more:
-            data = self._get(_url, params)
-            _memberships = data.get('organization_memberships')
-            yield from [ZendeskOrganizationMembership(self, i) for i in _memberships]
-            has_more = data.get('meta').get('has_more')
-            params.update({
-                'page[after]': data.get('meta').get('after_cursor'),
-            })
+        if self._organization_memberships is None:
+            result = []
+            url = f'{self.base_url}/organization_memberships.json'
+            params = {
+                'page[size]': 100,
+            }
+            has_more = True
+            while has_more:
+                data = self._get(url, params)
+                _memberships = data.get('organization_memberships')
+                result.extend([ZendeskOrganizationMembership(self, i) for i in _memberships])
+                has_more = data.get('meta').get('has_more')
+                params.update({
+                    'page[after]': data.get('meta').get('after_cursor'),
+                })
+            self._organization_memberships = result
+        return self._organization_memberships
 
     @property
     def organizations(self):
-        _url = f'{self.base_url}/organizations.json'
-        while _url is not None:
-            data = self._get(_url)
-            _url = data.get('next_page')
-            _orgs = data.get('organizations')
-            yield from [ZendeskOrganization(self, i) for i in _orgs]
+        if self._organizations is None:
+            log.debug('Fetching organization list for this client')
+            result = []
+            url = f'{self.base_url}/organizations.json'
+            params = {
+                'page[size]': 100,
+            }
+            has_more = True
+            while has_more:
+                data = self._get(url, params)
+                orgs = data.get('organizations')
+                result.extend([ZendeskOrganization(self, o) for o in orgs])
+                has_more = data.get('meta').get('has_more')
+                params.update({
+                    'page[after]': data.get('meta').get('after_cursor'),
+                })
+            self._organizations = result
+        return self._organizations
 
     def search(self, query: str, sort_by: str = None, sort_order: str = 'desc'):
         _url = f'{self.base_url}/search.json'
@@ -109,6 +199,10 @@ class ZendeskClient:
             _tickets = data.get('tickets')
             yield from [ZendeskTicket(self, i) for i in _tickets]
 
+    def unassign_organization(self, user_id: int, organization_id: int):
+        url = f'{self.base_url}/users/{user_id}/organizations/{organization_id}.json'
+        return self._delete(url)
+
     def update_organization(self, org_id: int, params: dict):
         _url = f'{self.base_url}/organizations/{org_id}.json'
         json = {'organization': params}
@@ -128,13 +222,24 @@ class ZendeskClient:
         return data
 
     @property
-    def users(self):
-        _url = f'{self.base_url}/users.json'
-        while _url is not None:
-            data = self._get(_url)
-            _url = data.get('next_page')
-            _users = data.get('users')
-            yield from [ZendeskUser(self, u) for u in _users]
+    def users(self) -> list['ZendeskUser']:
+        if self._users is None:
+            result = []
+            url = f'{self.base_url}/users.json'
+            params = {
+                'page[size]': 100,
+            }
+            has_more = True
+            while has_more:
+                data = self._get(url, params)
+                _users = data.get('users')
+                result.extend([ZendeskUser(self, u) for u in _users])
+                has_more = data.get('meta').get('has_more')
+                params.update({
+                    'page[after]': data.get('meta').get('after_cursor'),
+                })
+            self._users = result
+        return self._users
 
 
 class ZendeskApiObject(dict):
@@ -165,7 +270,40 @@ class ZendeskCustomFieldOption(ZendeskApiObject):
         return self.get('value')
 
 
+class ZendeskGroup(ZendeskApiObject):
+    @property
+    def default(self):
+        return self.get('default')
+
+    @property
+    def deleted(self):
+        return self.get('deleted')
+
+    @property
+    def description(self):
+        return self.get('description')
+
+    @property
+    def is_public(self):
+        return self.get('is_public')
+
+    @property
+    def name(self):
+        return self.get('name')
+
+
 class ZendeskOrganization(ZendeskApiObject):
+    _memberships = None
+
+    def __str__(self):
+        return self.name
+
+    @property
+    def memberships(self) -> list['ZendeskOrganizationMembership']:
+        if self._memberships is None:
+            self._memberships = list(self.client.list_memberships_for_org(self.id))
+        return self._memberships
+
     @property
     def name(self) -> str:
         return self.get('name')
@@ -175,6 +313,10 @@ class ZendeskOrganization(ZendeskApiObject):
         params = dict(name=value)
         self.update(params)
         self.client.update_organization(self.id, params)
+
+    @property
+    def users(self) -> list['ZendeskUser']:
+        return [self.client.get_user_by_id(m.user_id) for m in self.memberships]
 
 
 class ZendeskOrganizationMembership(ZendeskApiObject):
@@ -193,6 +335,7 @@ class ZendeskOrganizationMembership(ZendeskApiObject):
     @property
     def user_id(self) -> int:
         return self.get('user_id')
+
 
 class ZendeskTicket(ZendeskApiObject):
     @property
@@ -232,6 +375,14 @@ class ZendeskTicketField(ZendeskCustomField):
 
 
 class ZendeskUser(ZendeskApiObject):
+    _organizations = None
+
+    def __str__(self):
+        return self.name
+
+    def add_org_membership(self, org: ZendeskOrganization):
+        self.client.create_organization_membership(self.id, org.id)
+
     @property
     def email(self) -> str:
         return self.get('email')
@@ -258,8 +409,42 @@ class ZendeskUser(ZendeskApiObject):
         return self.get('name')
 
     @property
+    def organization_id(self) -> int:
+        return self.get('organization_id')
+
+    @property
+    def organizations(self) -> list[ZendeskOrganization]:
+        if self._organizations is None:
+            result = []
+            for m in self.client.list_memberships_for_user(self.id):
+                result.append(self.client.get_organization_by_id(m.organization_id))
+            self._organizations = result
+        return self._organizations
+
+    @property
+    def restricted_agent(self) -> bool:
+        return self.get('restricted_agent')
+
+    @property
+    def role(self) -> str:
+        return self.get('role')
+
+    @property
     def suspended(self) -> bool:
         return self.get('suspended')
+
+    @property
+    def ticket_restriction(self) -> str:
+        return self.get('ticket_restriction')
+
+    @ticket_restriction.setter
+    def ticket_restriction(self, value: str):
+        if value in ('assigned', 'groups', 'organization', 'requested', None):
+            response = self.client.update_user(self.id, {'ticket_restriction': value})
+            self.update(response.get('user'))
+
+    def unassign_organization(self, org: ZendeskOrganization):
+        self.client.unassign_organization(self.id, org.id)
 
 
 class ZendeskUserIdentity(ZendeskApiObject):
