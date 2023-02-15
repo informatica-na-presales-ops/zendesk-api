186dbb79650ef5238caf95cd7b95b3a024b0c44b
William Jackson <565174+williamjacksn@users.noreply.github.com>
2023-02-15 10:50:25 -0600

    Update get-people to include more data

--- get-people.py
+++ get-people.py
@@ -6,16 +6,21 @@ import zendesk
 
 def main():
     csv_writer = csv.writer(sys.stdout)
-    csv_writer.writerow(['user_id', 'name', 'email', 'organizations', 'role', 'restricted_agent', 'ticket_restriction'])
+    csv_writer.writerow([
+        'user_id', 'name', 'employee_id', 'active', 'verified', 'last_login_at', 'email', 'organizations', 'role',
+        'agent_groups', 'restricted_agent', 'ticket_restriction', 'suspended'
+    ])
     company = os.getenv('ZENDESK_COMPANY')
     username = os.getenv('ZENDESK_USERNAME')
     password = os.getenv('ZENDESK_PASSWORD')
     z = zendesk.ZendeskClient(company, username, password)
 
     for user in z.users:
-        org_names = '|'.join([o.name for o in user.organizations])
+        org_names = '|'.join(sorted([o.name for o in user.organizations]))
+        group_names = '|'.join(sorted([g.name for g in user.groups]))
         csv_writer.writerow([
-            user.id, user.name, user.email, org_names, user.role, user.restricted_agent, user.ticket_restriction
+            user.id, user.name, user.external_id, user.active, user.verified, user.last_login_at, user.email, org_names,
+            user.role, group_names, user.restricted_agent, user.ticket_restriction, user.suspended
         ])
 
 
